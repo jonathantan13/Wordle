dcce160cd45597601e3469ce48c3173ab8815608
jonathan <jtan13122004@gmail.com>
2025-08-29 21:52:52 +0800

    Created row input functionality

--- app.js
+++ app.js
@@ -1,20 +1,61 @@
+'use strict';
+
 const guessesContainer = document.querySelector('.guesses-container');
 const keyboard = document.querySelector('.keyboard');
 
 const GUESS_ROWS = 6;
+let row = 1;
+let column = 1;
 
-let rowsString = '';
+let guessRowEl, guessColumnEl;
 
-for (let i = 0; i < GUESS_ROWS; i++) {
-  rowsString += `
+const init = function () {
+  let rowsString = '';
+
+  // Dynamically generate 6 rows
+  for (let i = 0; i < GUESS_ROWS; i++) {
+    rowsString += `
         <div class="row-guesses row-${i + 1}">
-          <div class="letter"></div>
-          <div class="letter"></div>
-          <div class="letter"></div>
-          <div class="letter"></div>
-          <div class="letter"></div>
+            <div class="letter col-1"></div>
+            <div class="letter col-2"></div>
+            <div class="letter col-3"></div>
+            <div class="letter col-4"></div>
+            <div class="letter col-5"></div>
         </div>
     `;
-}
+  }
 
-guessesContainer.innerHTML = rowsString;
+  guessesContainer.innerHTML = rowsString;
+};
+
+const updateUI = function () {
+  guessRowEl = document.querySelector(`.row-${row}`);
+  guessColumnEl = guessRowEl.children[column - 1];
+};
+
+init();
+
+// TODO: Separate the guesses and adding/removing letters
+keyboard.addEventListener('click', (e) => {
+  // debugger;
+  e.preventDefault();
+
+  const letterEl = e.target;
+  const dataset = letterEl.dataset.letter;
+
+  if (!letterEl.hasAttribute('data-letter')) return;
+
+  updateUI();
+
+  // Adding letters
+  if (column <= 5 && dataset !== 'backspace' && dataset !== 'enter') {
+    guessColumnEl.append(letter.textContent);
+    if (column < 5) column++;
+  }
+
+  // Removing letters
+  if (dataset === 'backspace' && column >= 1) {
+    guessColumnEl.innerHTML = '';
+    if (column > 1) column--;
+  }
+});
